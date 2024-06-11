ad18f3287bec654047ad0cc599abc6218e671300
NICOLE BRUNSTEIN <47492985@ort>
2024-06-11 10:13:15 -0300

    11/06

---
+++ app/Quienes-somos/page.js
@@ -0,0 +1,12 @@
+function Quienessomos() {
+
+  
+    return (
+      <>
+        <h1>Somos Nicole y Ulises </h1> 
+
+      </>
+    );
+  }
+
+  export default Quienessomos;
---
+++ app/Reservas/page.js
@@ -0,0 +1,11 @@
+function reserva() {
+
+  
+    return (
+      <>
+        <h1>Reserva</h1> 
+      </>
+    );
+  }
+
+  export default reserva;
