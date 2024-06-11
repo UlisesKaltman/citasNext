1f504373fc0be6968f5561161f7324b148e4a548
NICOLE BRUNSTEIN <47492985@ort>
2024-06-11 10:28:59 -0300

    dwqbdj

--- app/Quienes-somos/page.js
+++ app/Quienes-somos/page.js
@@ -3,7 +3,29 @@ function Quienessomos() {
   
     return (
       <>
-        <h1>Somos Nicole y Ulises </h1> 
+        <div class="Nosotros">
+        <div class="card" >
+            <image src="https://media.istockphoto.com/id/528877033/es/foto/adolescente-en-un-traje.jpg?s=612x612&w=0&k=20&c=ZfIly03y3UrObkCMmSFVcacKfMQYwvUGwns6MOAgFgE=" class="card-img-top" alt="..."/>
+        <div class="card-body">
+                <h5 class="card-title">Ulises Kaltman</h5>
+                 <p class="card-text">
+   
+                Ulises Kaltman, el visionario detrás de nuestro proyecto, destaca por su aguda perspicacia y creatividad innovadora.
+                Como ideólogo principal, Ulises concibió la visión que impulsa nuestro proyecto, demostrando una habilidad excepcional para identificar
+                oportunidades únicas y traducirlas en iniciativas impactantes.</p>
+  </div>
+</div>
+
+
+<div class="card" >
+  <image src="https://insademexico.mx/wp-content/uploads/2017/11/Traje-sastre-para-damas-683x1024.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Nicole Brunstein </h5>
+    <p class="card-text">
+Nicole Brunstein, la mente técnica detrás de nuestro proyecto, se destaca por su excepcional habilidad para traducir la visión conceptual en una realidad funcional. Como programadora principal, Nicole ha demostrado una destreza impresionante en el desarrollo de código limpio y eficiente, garantizando la robustez y la eficacia de nuestra plataforma.</p>
+  </div>
+</div>
+</div>
 
       </>
     );
